1a05e914e4d1d9ee7ed755439ab2bf39bdbd99ad
Ori Hoch <ori@uumpa.com>
2021-04-26 18:49:49 +0300

    check and fix private ssh key file permissions before using

--- open_bus_siri_requester/requester.py
+++ open_bus_siri_requester/requester.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import requests
@@ -14,6 +15,11 @@ REQUEST_URL_TEMPLATE = "http://moran.mot.gov.il:110/Channels/HTTPChannel/SmQuery
 def start_ssh_tunnel():
     if config.OPEN_BUS_SSH_TUNNEL_PRIVATE_KEY_FILE and config.OPEN_BUS_SSH_TUNNEL_SERVER_IP:
         print("Starting Open Bus SSH Tunnel ({})".format(config.OPEN_BUS_SSH_TUNNEL_SERVER_IP), file=sys.stderr)
+        filemode = oct(os.stat(config.OPEN_BUS_SSH_TUNNEL_PRIVATE_KEY_FILE).st_mode)[-3:]
+        if filemode not in ['600', '400']:
+            print("Changing key file permissions", file=sys.stderr)
+            ret, out = subprocess.getstatusoutput('chmod 400 {}'.format(config.OPEN_BUS_SSH_TUNNEL_PRIVATE_KEY_FILE))
+            assert ret == 0, out
         p = subprocess.Popen([
             'ssh', '-o', 'UserKnownHostsFile=/dev/null', '-o', 'StrictHostKeyChecking=no',
             '-D', '127.0.0.1:8123', '-C', '-N', '-i', config.OPEN_BUS_SSH_TUNNEL_PRIVATE_KEY_FILE,
